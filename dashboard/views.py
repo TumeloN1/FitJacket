1f1509c8f4e92bdddf79793dd9467cdc2227f549
vivvzhou <vivywzhou@gmail.com>
2025-04-14 15:29:31 -0400

    rearranged dashboard and fixed some stuff.?

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,16 +2,23 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from goals.models import FitnessGoal
 from workouts.models import WorkoutLog
+from django.utils.timezone import now 
 
 @login_required
 def view_dashboard(request):
     first_name = request.user.username
     goals = FitnessGoal.objects.filter(user=request.user)
-    logs = WorkoutLog.objects.filter(user=request.user).order_by('-date')[:2]
+    logs = WorkoutLog.objects.filter(user=request.user).filter(date=now().date())
+    chart_data = {
+        'dates': [log.date.strftime('%Y-%m-%d') for log in WorkoutLog.objects.filter(user=request.user)],
+        'duration': [log.duration for log in WorkoutLog.objects.filter(user=request.user)],
+    }
+    print(chart_data)
     return render(request, 'dashboard/dashboard.html' , {
         'first_name': first_name,
         'goals': goals,
         'logs': logs,
+        'chart_data': chart_data,
     })
 
 # Create your exercise_views here.
