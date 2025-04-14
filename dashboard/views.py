27a15de0bf2936d3377b75b061a7b7213f708d38
vivvzhou <vivywzhou@gmail.com>
2025-04-13 23:00:05 -0400

    rearranged dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from goals.models import FitnessGoal
+from workouts.models import WorkoutLog
 
 @login_required
 def view_dashboard(request):
     first_name = request.user.username
     goals = FitnessGoal.objects.filter(user=request.user)
+    logs = WorkoutLog.objects.filter(user=request.user).order_by('-date')[:2]
     return render(request, 'dashboard/dashboard.html' , {
         'first_name': first_name,
         'goals': goals,
+        'logs': logs,
     })
 
 # Create your exercise_views here.
