aaa2c34962c945a884b57594e6a51667c22216e6
vivvzhou <vivywzhou@gmail.com>
2025-04-13 21:11:53 -0400

    basic dashboard ui

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def view_dashboard(request):
-    return render(request, 'dashboard/dashboard.html')
+    first_name = request.user.username
+    return render(request, 'dashboard/dashboard.html' , {'first_name': first_name})
 
 # Create your exercise_views here.
