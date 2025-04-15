b93dcbf34e9092a3c31b3aec69412bebdaa253f8
vivvzhou <vivywzhou@gmail.com>
2025-04-14 21:51:38 -0400

    dashboard ui small changes and logic

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,13 +2,19 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from goals.models import FitnessGoal
 from workouts.models import WorkoutLog
-from django.utils.timezone import now 
+from django.utils.timezone import now, timedelta
+from django.db.models import Count
 
 @login_required
 def view_dashboard(request):
     first_name = request.user.username
     goals = FitnessGoal.objects.filter(user=request.user)
     logs = WorkoutLog.objects.filter(user=request.user).filter(date=now().date())
+    today = now().date()
+    start_of_week = today - timedelta(days=today.weekday())
+
+    logs = WorkoutLog.objects.filter(user=request.user, date__gte=start_of_week)
+    active_days = logs.values('date').distinct().count()
     chart_data = {
         'dates': [log.date.strftime('%Y-%m-%d') for log in WorkoutLog.objects.filter(user=request.user)],
         'duration': [log.duration for log in WorkoutLog.objects.filter(user=request.user)],
@@ -19,6 +25,7 @@ def view_dashboard(request):
         'goals': goals,
         'logs': logs,
         'chart_data': chart_data,
+        'active_days': active_days,
     })
 
 # Create your exercise_views here.
